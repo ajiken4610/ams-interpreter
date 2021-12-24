f62ea368f413fa4e984ec40585f3e95de3ea19f8
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2021-12-24 21:57:14 +0900

    2021/12/24コミットメント

--- public/index.html.ts
+++ public/index.html.ts
@@ -6,18 +6,25 @@ import "./index.html.css";
 $(() => {
   WindowLib.ToastUtils.showToast("Hello!!");
 
-  console.log("====variablemap====");
-  let map1 = new AMS.AMSVariableMap<string>();
-  map1.set("a", "hello");
-  let map2 = new AMS.AMSVariableMap<string>(map1);
-  map2.set("b", "world");
-  console.log(map2.toString());
-  map2.update("a", "another");
-  console.log(map2.toString());
-  map2.update("a", "world");
-  map2.set("a", "another");
-  console.log(map1.toString());
-  console.log(map2.toString());
+  // console.log("====variablemap====");
+  // let map1 = new AMS.AMSVariableMap<string>();
+  // map1.set("a", "hello");
+  // let map2 = new AMS.AMSVariableMap<string>(map1);
+  // map2.set("b", "world");
+  // map2.set("a", "another");
+  // map2.set("c", "!!!");
+  // console.log(map2.toString());
 
-  console.log("====stringiterator====");
+  console.log("====absamsobject====");
+  let parsed1 = AMS.AMSParser.parseAMS(
+    `
+AAA;BBB{
+  XXX;
+  YYY;
+  ZZZ
+}CCC;
+/DDD{10}
+`
+  ).toString();
+  console.log(parsed1);
 });
